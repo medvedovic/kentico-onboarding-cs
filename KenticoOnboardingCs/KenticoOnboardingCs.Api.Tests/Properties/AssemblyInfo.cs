7eaa68f5c69e265cc2876a55f4ace8e9bb027df5
Patrik Medveď <PatrikM@kentico.com>
2017-08-15 13:48:03 +0200

    KA-175 Create solution

---
+++ KenticoOnboardingCs/KenticoOnboardingCs.Api.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KenticoOnboardingCs.Api.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kentico software")]
+[assembly: AssemblyProduct("KenticoOnboardingCs.Api.Tests")]
+[assembly: AssemblyCopyright("Copyright © Kentico software 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("561fdb26-184d-4cbd-b846-ac2b2f69c442")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KenticoOnboardingCs/KenticoOnboardingCs.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KenticoOnboardingCs.Api.WebApiApplication" Language="C#" %>
