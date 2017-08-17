2cceb2b0badb264e2a5390a1544b7c8af1641c90
Patrik Medveď <PatrikM@kentico.com>
2017-08-17 14:31:51 +0200

    KA-176 Create get in dummy controller

---
+++ KenticoOnboardingCs/KenticoOnboardingCs.Api/Controllers/DummyController.cs
@@ -0,0 +1,48 @@
+﻿using KenticoOnboardingCs.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KenticoOnboardingCs.Api.Controllers
+{
+    public class DummyController : ApiController
+    {
+        private IEnumerable<Todo> todos;
+
+        public DummyController()
+        {
+            todos = new List<Todo>()
+            {
+                new Todo() { Id = 1, Name = "Make coffee", Done = true },
+                new Todo() { Id = 2, Name = "Master ASP.NET web api", Done = false }
+            };
+        }
+
+        public DummyController(IEnumerable<Todo> todos)
+        {
+            if(todos != null)
+            {
+                this.todos = todos;
+            }
+        }
+
+
+        public IEnumerable<Todo> GetAllTodos()
+        {
+            return todos;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetTodo(int id)
+        {
+            var todo = todos.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+                return NotFound();
+
+            return Ok(todo);
+        }
+    }
+}
