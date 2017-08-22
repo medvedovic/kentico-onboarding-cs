a9d88ec434405c910e792a3fa78a2c107cb457cc
Patrik Medveď <PatrikM@kentico.com>
2017-08-22 11:29:33 +0200

    KA-176 Edit controller using mock repo

--- KenticoOnboardingCs/KenticoOnboardingCs.Api/Controllers/DummyController.cs
+++ KenticoOnboardingCs/KenticoOnboardingCs.Api/Controllers/DummyController.cs
@@ -1,48 +1,61 @@
 ﻿using KenticoOnboardingCs.Api.Models;
+using KenticoOnboardingCs.Api.Models.Repositories;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace KenticoOnboardingCs.Api.Controllers
 {
     public class DummyController : ApiController
     {
-        private IEnumerable<Todo> todos;
+        private ITodoRepository _repository;
+
+        public DummyController(ITodoRepository todoRepository)
+        {
+            _repository = todoRepository;
+        }
 
         public DummyController()
         {
-            todos = new List<Todo>()
-            {
-                new Todo() { Id = 1, Name = "Make coffee", Done = true },
-                new Todo() { Id = 2, Name = "Master ASP.NET web api", Done = false }
-            };
+            _repository = new TodoRepository();
         }
 
-        public DummyController(IEnumerable<Todo> todos)
-        {
-            if(todos != null)
-            {
-                this.todos = todos;
-            }
-        }
-
-
+        //Get api/dummy/
+        [HttpGet]
         public IEnumerable<Todo> GetAllTodos()
         {
-            return todos;
+            return _repository.GetAll();
         }
 
+        //Get api/dummy/{id}
         [HttpGet]
         public IHttpActionResult GetTodo(int id)
         {
-            var todo = todos.FirstOrDefault(t => t.Id == id);
+            var todo = _repository.Get(id);
             if (todo == null)
                 return NotFound();
 
             return Ok(todo);
+        }
+
+        //Post api/dummy/
+        [HttpPost]
+        public IHttpActionResult PostTodo(Todo todo)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var newTodo =_repository.Add(todo);
+                return Ok(newTodo);
+            }
+            catch(Exception ex)
+            {
+                return Conflict();
+            }
         }
     }
 }
