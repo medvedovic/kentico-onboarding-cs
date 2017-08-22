80ff21d1a81667d90e21a5d31e43f45e5fa1865c
Patrik Medveď <PatrikM@kentico.com>
2017-08-22 11:28:33 +0200

    KA-176 Create mock todo repository

---
+++ KenticoOnboardingCs/KenticoOnboardingCs.Api/Models/Repositories/ITodoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenticoOnboardingCs.Api.Models.Repositories
+{
+    public interface ITodoRepository
+    {
+        IEnumerable<Todo> GetAll();
+        Todo Get(int id);
+        Todo Add(Todo item);
+        void Remove(int id);
+        bool Update(Todo item);
+    }
+}
---
+++ KenticoOnboardingCs/KenticoOnboardingCs.Api/Models/Repositories/TodoRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KenticoOnboardingCs.Api.Models.Repositories
+{
+    public class TodoRepository : ITodoRepository
+    {
+        private List<Todo> todos;
+        private int nextId = 3;
+        public TodoRepository()
+        {
+            todos = new List<Todo>()
+            {
+                new Todo() { Id = 1, Name = "Make coffee", Done = true },
+                new Todo() { Id = 2, Name = "Master ASP.NET web api" }
+            };
+        }
+
+        public IEnumerable<Todo> GetAll()
+        {
+            return todos;
+        }
+
+        public Todo Get(int id)
+        {
+            return todos
+                .Find(todo => todo.Id == id);
+        }
+
+        public Todo Add(Todo todo)
+        {
+            if (todo == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            todo.Id = nextId++;
+            todos.Add(todo);
+
+            return todo;
+        }
+
+
+        public void Remove(int id)
+        {
+            todos.RemoveAll(todo => todo.Id == id);
+        }
+
+        public bool Update(Todo todo)
+        {
+            if (todo == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            int index = todos.FindIndex(p => p.Id == todo.Id);
+            if (index == -1)
+            {
+                return false;
+            }
+            todos.RemoveAt(index);
+            todos.Add(todo);
+            return true;
+        }
+    }
+}
