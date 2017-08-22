0b150d20de34170871583d6d3c7fc53328056de4
Patrik Medveď <PatrikM@kentico.com>
2017-08-22 11:39:14 +0200

    KA-176 Create post in dummy controller

---
+++ KenticoOnboardingCs/KenticoOnboardingCs.Api.Tests/Controllers/DummyControllerTests_Post.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using KenticoOnboardingCs.Api.Models;
+using KenticoOnboardingCs.Api.Controllers;
+using System.Web.Http.Results;
+using KenticoOnboardingCs.Api.Models.Repositories;
+using System.ComponentModel.DataAnnotations;
+
+namespace KenticoOnboardingCs.Api.Tests.Controllers
+{
+    [TestFixture]
+    class DummyControllerTests_Post
+    {
+        private DummyController dummyController;
+        private ITodoRepository repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            repository = new TodoRepository();
+            dummyController = new DummyController(repository);
+        }
+
+        [Test]
+        public void PostDummyItem_ValidatesModelCorrectly()
+        {
+            // Arrange
+            var newTodo = new Todo();
+            var context = new ValidationContext(newTodo);
+            var results = new List<ValidationResult>();
+
+            //act
+            var isModelStateValid = Validator.TryValidateObject(newTodo, context, results);
+
+            //assert
+            Assert.NotZero(results.Count);
+            Assert.IsFalse(isModelStateValid);
+        }
+
+        [Test]
+        public void PostDummyItem_ReturnsNewTodo_WithValidModel()
+        {
+            var itemToPost = new Todo() { Name = "Go home" };
+
+            var response = dummyController.PostTodo(itemToPost);
+
+            Assert.IsInstanceOf<OkNegotiatedContentResult<Todo>>(response);
+        }
+
+        [Test]
+        public void PostDummyItem_ReturnsError_WithInvalidModel()
+        {
+            var itemToPost = new Todo();
+
+            dummyController.ModelState.AddModelError("test", "test");
+            var response = dummyController.PostTodo(itemToPost);
+
+            Assert.IsInstanceOf<InvalidModelStateResult>(response);
+        }
+    }
+}
