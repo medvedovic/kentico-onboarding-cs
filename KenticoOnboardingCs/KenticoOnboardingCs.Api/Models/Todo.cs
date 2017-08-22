6a82e0bd52ba4955722bd842953c09fe9035fabf
Patrik Medveď <PatrikM@kentico.com>
2017-08-22 11:27:04 +0200

    KA-176 Add data validation to model

--- KenticoOnboardingCs/KenticoOnboardingCs.Api/Models/Todo.cs
+++ KenticoOnboardingCs/KenticoOnboardingCs.Api/Models/Todo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace KenticoOnboardingCs.Api.Models
 {
     public class Todo
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
-        public bool Done { get; set; }
+        public bool Done { get; set; } = false;
     }
 }
