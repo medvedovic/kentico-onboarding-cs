30a1730ae130608b1389f64dd74ecfdf39b68af3
Patrik Medveď <PatrikM@kentico.com>
2017-09-06 14:16:14 +0200

    KA-176 Create uri helper

---
+++ TodoApp/TodoApp.Api/Helpers/IUriHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Api.Helpers
+{
+    public interface IUriHelper
+    {
+        Uri BuildUri(HttpRequestMessage request, Guid id);
+    }
+}
---
+++ TodoApp/TodoApp.Api/Helpers/UriHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TodoApp.Api.Helpers
+{
+    public class UriHelper : IUriHelper
+    {
+        public Uri BuildUri(HttpRequestMessage request, Guid id)
+        {
+            return new Uri(request.RequestUri + id.ToString());
+        }
+    }
+}
