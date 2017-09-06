88c10b13741716476eb575470ca784e4ae3efcb0
Patrik Medveď <PatrikM@kentico.com>
2017-09-06 10:10:44 +0200

    KA-176 Change todo to use guid as id

--- TodoApp/TodoApp.Api/Models/Todo.cs
+++ TodoApp/TodoApp.Api/Models/Todo.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TodoApp.Api.Models
 {
     public class Todo
     {
         [Key]
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         [Required]
         [MaxLength(255)]
         public string Value { get; set; }
