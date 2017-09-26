58de6fb8aaffdbbd2ae5ded3c238589fc21f43c3
Patrik Medveď <PatrikM@kentico.com>
2017-09-26 10:44:59 +0200

    KA-177 Create ApiDependencyBootstrapper

---
+++ TodoApp/src/TodoApp.Api/ApiDependencyBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Web;
+using Microsoft.Practices.Unity;
+using TodoApp.Api.Helpers;
+using TodoApp.Contracts;
+using TodoApp.Contracts.Helpers;
+
+namespace TodoApp.Api
+{
+    public class ApiDependencyBootstrapper: IUnityBootstrapper
+    {
+        public void RegisterType(IUnityContainer container)
+        {
+            container.RegisterType<HttpRequestMessage>(
+                new HierarchicalLifetimeManager(), 
+                new InjectionFactory(GetHttpRequestMessage)
+            );
+            container.RegisterType<IUriHelper, UriHelper>(new ContainerControlledLifetimeManager());
+        }
+
+        private static HttpRequestMessage GetHttpRequestMessage(IUnityContainer container) =>
+            (HttpRequestMessage) HttpContext.Current.Items["MS_HttpRequestMessage"];
+    }
+}
