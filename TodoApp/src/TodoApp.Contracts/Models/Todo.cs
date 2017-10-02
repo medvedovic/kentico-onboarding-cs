a11cfa0c9c7f547399eca17360de0dde6f29f71f
Patrik Medveď <PatrikM@kentico.com>
2017-10-02 15:38:53 +0200

    KA-177 Simplify todo model

--- TodoApp/src/TodoApp.Contracts/Models/Todo.cs
+++ TodoApp/src/TodoApp.Contracts/Models/Todo.cs
@@ -5,26 +5,9 @@ namespace TodoApp.Contracts.Models
 {
     public class Todo
     {
-        [Key]
         public Guid Id { get; set; }
-        [Required]
-        [MaxLength(255)]
         public string Value { get; set; }
-
         public DateTime CreatedAt { get; set; }
-
         public DateTime? UpdatedAt { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return ((obj is Todo)
-                && Id == ((Todo)obj).Id
-                && Value == ((Todo) obj).Value);
-        }
-
-        public override int GetHashCode()
-        {
-            return Id.GetHashCode();
-        }
     }
 }
