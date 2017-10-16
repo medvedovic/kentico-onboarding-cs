fb1cf312f5fcfea15e9d0d8c871312488f25f74f
Patrik Medveď <PatrikM@kentico.com>
2017-10-16 14:06:17 +0200

    KA-178 Add BsonId attribute to todo model

--- TodoApp/src/TodoApp.Contracts/Models/Todo.cs
+++ TodoApp/src/TodoApp.Contracts/Models/Todo.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace TodoApp.Contracts.Models
 {
     public class Todo
     {
+        [BsonId]
         public Guid Id { get; set; }
         public string Value { get; set; }
         public DateTime CreatedAt { get; set; }
