2ffe4a5e7e9391ed8ad82d73aa010a066ed4114d
Patrik Medveď <PatrikM@kentico.com>
2017-10-17 10:19:09 +0200

    KA-178 Create PostTodoService

---
+++ TodoApp/src/TodoApp.Services/Todos/PostTodoService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using TodoApp.Contracts.Dtos;
+using TodoApp.Contracts.Models;
+using TodoApp.Contracts.Repositories;
+using TodoApp.Contracts.Services.Todos;
+
+namespace TodoApp.Services.Todos
+{
+    class PostTodoService: IPostTodoService
+    {
+        private readonly ITodoRepository _repository;
+
+        public PostTodoService(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Todo> CreateTodoAsync(TodoDto todoDto)
+        {
+            // validate model
+
+            var newTodo = new Todo
+            {
+                Id = Guid.NewGuid(),
+                Value = todoDto.Value,
+                CreatedAt = DateTime.Now
+            };        
+
+            return await _repository.CreateAsync(newTodo);
+        }
+    }
+}
