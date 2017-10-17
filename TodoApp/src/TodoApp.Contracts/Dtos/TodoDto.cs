2191f454e15e35fa2216e2cf2f01ab72791cd519
Patrik Medveď <PatrikM@kentico.com>
2017-10-17 10:54:37 +0200

    KA-178 Move dto to contracts

--- TodoApp/src/TodoApp.Contracts/Dtos/TodoDto.cs
+++ TodoApp/src/TodoApp.Contracts/Dtos/TodoDto.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
-namespace TodoApp.Api.Dtos
+namespace TodoApp.Contracts.Dtos
 {
     public class TodoDto
     {
         public Guid? Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Value { get; set; }
     }
 }
