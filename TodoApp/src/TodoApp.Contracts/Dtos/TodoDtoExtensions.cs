5cfdde274012a60f3a12b3b8e4bb10fdcfebfdde
Patrik Medveď <PatrikM@kentico.com>
2017-10-17 14:00:14 +0200

    KA-178 Create converter from dto to todo

---
+++ TodoApp/src/TodoApp.Contracts/Dtos/TodoDtoExtensions.cs
@@ -0,0 +1,13 @@
+﻿using TodoApp.Contracts.Models;
+
+namespace TodoApp.Contracts.Dtos
+{
+    public static class TodoDtoExtensions
+    {
+        public static Todo ToTodo(this TodoDto dto) =>
+            new Todo
+            {
+                Value = dto.Value
+            };
+    }
+}
