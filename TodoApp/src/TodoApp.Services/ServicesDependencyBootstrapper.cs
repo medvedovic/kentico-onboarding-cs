4784997011f8f6d3c2cebf5786293b37d3e9b3a4
Patrik Medveď <PatrikM@kentico.com>
2017-10-17 11:33:01 +0200

    KA-178 Inject ServiceHelper in PostTodoService

--- TodoApp/src/TodoApp.Services/ServicesDependencyBootstrapper.cs
+++ TodoApp/src/TodoApp.Services/ServicesDependencyBootstrapper.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.Practices.Unity;
 using TodoApp.Contracts;
+using TodoApp.Contracts.Helpers;
 using TodoApp.Contracts.Services.Todos;
+using TodoApp.Services.Helpers;
 using TodoApp.Services.Todos;
 
 namespace TodoApp.Services
 {
-    public class ServicesDependencyBootstrapper: IUnityBootstrapper
+    public class ServicesDependencyBootstrapper : IUnityBootstrapper
     {
-        public IUnityContainer RegisterType(IUnityContainer container, DependencyBootstrapperConfig configuration = null) => 
-            container.RegisterType<IPostTodoService, PostTodoService>(new ContainerControlledLifetimeManager());
+        public IUnityContainer RegisterType(IUnityContainer container,DependencyBootstrapperConfig configuration = null) =>
+            container
+                .RegisterType<IPostTodoService, PostTodoService>(new ContainerControlledLifetimeManager())
+                .RegisterType<IServiceHelper, ServiceHelper>(new ContainerControlledLifetimeManager());
     }
 }
