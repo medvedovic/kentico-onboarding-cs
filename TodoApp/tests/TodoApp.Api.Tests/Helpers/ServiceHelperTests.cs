15af4095373682f7501a866fbbcd3b5ba1db73f2
Patrik Medveď <PatrikM@kentico.com>
2017-10-17 13:59:22 +0200

    KA-178 Create tests for ServiceHelper

---
+++ TodoApp/tests/TodoApp.Api.Tests/Helpers/ServiceHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using TodoApp.Contracts.Helpers;
+using TodoApp.Services.Helpers;
+
+namespace TodoApp.Api.Tests.Helpers
+{
+    [TestFixture]
+    class ServiceHelperTests
+    {
+        private IServiceHelper _serviceHelper;
+
+        [SetUp]
+        public void Init()
+        {
+            _serviceHelper = new ServiceHelper();
+        }
+
+        [Test]
+        public void ReturnsDifferentGuids()
+        {
+            var guid1 = _serviceHelper.GenerateGuid();
+            var guid2 = _serviceHelper.GenerateGuid();
+
+            Assert.That(guid1, Is.Not.EqualTo(guid2));
+        }
+
+        [Test]
+        public void ReturnsDifferentDateTimes()
+        {
+            var time1 = _serviceHelper.GetCurrentDateTime();
+            var time2 = _serviceHelper.GetCurrentDateTime();
+
+            Assert.That(time1, Is.Not.EqualTo(time2));
+        }
+    }
+}
