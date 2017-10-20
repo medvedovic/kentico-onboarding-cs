15120107f4ff1ea4710dca594079e2e266f11596
Patrik Medveď <PatrikM@kentico.com>
2017-10-20 10:58:45 +0200

    KA-178 Write tests for PutTodoService

---
+++ TodoApp/tests/TodoApp.Api.Tests/Services/Todos/PutTodoServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NUnit.Framework;
+using TodoApp.Api.Tests.Helpers;
+using TodoApp.Api.ViewModels;
+using TodoApp.Contracts.Helpers;
+using TodoApp.Contracts.Models;
+using TodoApp.Contracts.Repositories;
+using TodoApp.Contracts.Services.Todos;
+using TodoApp.Services.Todos;
+
+namespace TodoApp.Api.Tests.Services.Todos
+{
+    [TestFixture]
+    class PutTodoServiceTests
+    {
+        private IPutTodoService _putTodoService;
+        private IServiceHelper _mockServiceHelper;
+        private ITodoRepository _mockTodoRepository;
+
+        [SetUp]
+        public void Init()
+        {
+            _mockTodoRepository = Substitute.For<ITodoRepository>();
+            _mockServiceHelper = Substitute.For<IServiceHelper>();
+            _putTodoService = new PutTodoService(_mockServiceHelper, _mockTodoRepository);
+        }
+
+        [Test]
+        public void UpdateTodoAsync_ThrowsException_OnTodoNotFound()
+        {
+            var guid = new Guid("128539cb-a41e-42a1-805b-1eb533e86461");
+            _mockTodoRepository.RetrieveAsync(guid).Throws(new ArgumentNullException());
+            
+            Assert.That(() => _putTodoService.UpdateTodoAsync(guid, new TodoViewModel()), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void UpdateTodoAsync_ReturnsCorrectTodo()
+        {
+            var guid = new Guid("128539cb-a41e-42a1-805b-1eb533e86461");
+            var todoViewModel = new TodoViewModel {Value = "Test PutTodoService"};
+            var returnedTodo = new Todo
+            {
+                Id = guid,
+                Value = "Test stuff",
+                CreatedAt = new DateTime(2017, 10, 17, 10, 00, 00)
+            };
+            var expectedResult = new Todo
+            {
+                Id = guid,
+                Value = "Test PutTodoService",
+                CreatedAt = new DateTime(2017, 10, 17, 10, 00, 00),
+                UpdatedAt = new DateTime(2017, 10, 21, 10, 44, 12)
+            };
+            _mockServiceHelper.GetCurrentDateTime().Returns(new DateTime(2017, 10, 21, 10, 44, 12));
+            _mockTodoRepository.RetrieveAsync(guid).Returns(returnedTodo);
+            _mockTodoRepository.UpdateAsync(Arg.Any<Todo>()).Returns(parameters => parameters.Arg<Todo>());
+
+            var result = _putTodoService.UpdateTodoAsync(guid, todoViewModel).Result;
+
+            Assert.That(result, Is.EqualTo(expectedResult).Using(new TodosEqualityComparer()));
+        }
+    }
+}
