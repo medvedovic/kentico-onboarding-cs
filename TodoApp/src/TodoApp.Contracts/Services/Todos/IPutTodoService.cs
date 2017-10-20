3e56578ba8d4cc3364a143f652b59bf38b76664a
Patrik Medveď <PatrikM@kentico.com>
2017-10-20 10:12:36 +0200

    KA-178 Create IPutTodoService

---
+++ TodoApp/src/TodoApp.Contracts/Services/Todos/IPutTodoService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using TodoApp.Contracts.Models;
+
+namespace TodoApp.Contracts.Services.Todos
+{
+    public interface IPutTodoService
+    {
+        Task<Todo> UpdateTodoAsync(Guid id, IConvertibleTo<Todo> todoViewModel);
+    }
+}
