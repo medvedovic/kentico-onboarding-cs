e2c7039d6d6c2c1967452f777edbd39521ffdb06
Patrik Medveď <PatrikM@kentico.com>
2017-10-24 15:41:43 +0200

    KA-178 Create IGetTodoService

---
+++ TodoApp/src/TodoApp.Contracts/Services/Todos/IGetTodoService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using TodoApp.Contracts.Models;
+
+namespace TodoApp.Contracts.Services.Todos
+{
+    public interface IGetTodoService
+    {
+        Todo CachedTodo { get; }
+
+        Task<Todo> RetrieveTodoAsync(Guid id);
+
+        Task<bool> IsTodoInDbAsync(Guid id);
+    }
+}
