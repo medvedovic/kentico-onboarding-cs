745eae078f46559707a9165ed27301ccc3b645fe
Patrik Medveď <PatrikM@kentico.com>
2017-11-02 15:46:48 +0100

    KA-178 Extract DatabaseConfig build to a separate file

--- TodoApp/src/TodoApp.Api/ApiDependencyBootstrapper.cs
+++ TodoApp/src/TodoApp.Api/ApiDependencyBootstrapper.cs
@@ -2,7 +2,6 @@
 using System.Web;
 using Microsoft.Practices.Unity;
 using TodoApp.Api.Helpers;
-using TodoApp.Contracts;
 using TodoApp.Contracts.Bootstrap;
 using TodoApp.Contracts.Helpers;
 
@@ -12,16 +11,9 @@ namespace TodoApp.Api
     {
         public IUnityContainer RegisterType(IUnityContainer container) => 
             container
-                .RegisterType<DatabaseConfig>(new HierarchicalLifetimeManager(), new InjectionFactory(GenerateDatabaseConfig))
+                .RegisterType<DatabaseConfig>(new HierarchicalLifetimeManager(), new InjectionFactory(DatabaseConfigGenerator.Generate))
                 .RegisterType<HttpRequestMessage>(new HierarchicalLifetimeManager(), new InjectionFactory(GetHttpRequestMessage))
                 .RegisterType<IUriHelper, UriHelper>(new HierarchicalLifetimeManager());
-
-        private static DatabaseConfig GenerateDatabaseConfig(IUnityContainer arg) => 
-            new DatabaseConfig
-            {
-                ConnectionString = System.Configuration.ConfigurationManager
-                    .ConnectionStrings["DefaultConnection"].ConnectionString
-            };
 
         private static HttpRequestMessage GetHttpRequestMessage(IUnityContainer container) =>
             (HttpRequestMessage) HttpContext.Current.Items["MS_HttpRequestMessage"];
---
+++ TodoApp/src/TodoApp.Api/DatabaseConfigGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Practices.Unity;
+using TodoApp.Contracts.Bootstrap;
+
+namespace TodoApp.Api
+{
+    internal class DatabaseConfigGenerator
+    {
+        internal static IDatabaseConfig Generate(IUnityContainer arg) => 
+            new DatabaseConfig
+            {
+                ConnectionString = System.Configuration.ConfigurationManager
+                    .ConnectionStrings["DefaultConnection"].ConnectionString
+            };
+    }
+}
