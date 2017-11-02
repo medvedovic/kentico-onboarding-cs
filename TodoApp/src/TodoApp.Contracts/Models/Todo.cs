f66713d9a8b84fb7503f88916d20fe1c523608c6
Patrik Medveď <PatrikM@kentico.com>
2017-11-02 17:17:51 +0100

    KA-178 Change UpdatedAt to non-nullable property in Todo

--- TodoApp/src/TodoApp.Contracts/Models/Todo.cs
+++ TodoApp/src/TodoApp.Contracts/Models/Todo.cs
@@ -7,6 +7,6 @@ namespace TodoApp.Contracts.Models
         public Guid Id { get; set; }
         public string Value { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
