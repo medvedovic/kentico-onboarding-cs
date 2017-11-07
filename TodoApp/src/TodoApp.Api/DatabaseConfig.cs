9709b34916f71930e6ab961d2000506c2c05699d
Patrik Medveď <PatrikM@kentico.com>
2017-11-07 14:02:41 +0100

    KA-178 Merge DatabaseConfigConstructor with DatabaseConfig

--- TodoApp/src/TodoApp.Api/DatabaseConfig.cs
+++ TodoApp/src/TodoApp.Api/DatabaseConfig.cs
@@ -1,9 +1,17 @@
 ﻿using TodoApp.Contracts.Bootstrap;
+using Unity;
 
 namespace TodoApp.Api
 {
     public class DatabaseConfig : IDatabaseConfig
     {
         public string ConnectionString { get; set; }
+
+        internal static IDatabaseConfig Create(IUnityContainer arg) =>
+            new DatabaseConfig
+            {
+                ConnectionString = System.Configuration.ConfigurationManager
+                    .ConnectionStrings["DefaultConnection"].ConnectionString
+            };
     }
 }
