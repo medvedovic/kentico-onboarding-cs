bc91d050b051075f0ef80bea58c8d06077a50a7f
Patrik Medveď <PatrikM@kentico.com>
2017-11-09 13:48:35 +0100

    KA-178 Reformat return expressions to lambdas

--- TodoApp/tests/TodoApp.Contracts.Base/EqualityComparer/TodosEqualityComparerExtensions.cs
+++ TodoApp/tests/TodoApp.Contracts.Base/EqualityComparer/TodosEqualityComparerExtensions.cs
@@ -4,9 +4,7 @@ namespace TodoApp.Contracts.Base.EqualityComparer
 {
     public static class TodosEqualityComparerExtensions
     {
-        public static EqualConstraint UsingTodosEqualityComparer(this EqualConstraint equalConstraint)
-        {
-            return equalConstraint.Using(TodosEqualityComparer.Instance);
-        }
+        public static EqualConstraint UsingTodosEqualityComparer(this EqualConstraint equalConstraint) => 
+            equalConstraint.Using(TodosEqualityComparer.Instance);
     }
 }
